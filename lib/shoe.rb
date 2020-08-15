2cf7312ab891100ab9ac2f7955ab137f77dac039
Yani Asari <yani.iman1@gmail.com>
2020-08-15 03:28:00 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,16 @@
 class Shoe
-  attr_accessor :color, :material, :condition
-  attr_reader :brand, :size
-  
-  brands = []
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
-    brands << brand 
-    brands.uniq!
+    if !(BRANDS.include?(@brand))
+      BRANDS << @brand
+    end
   end
+
 
   def cobble
     self.condition = "new"
